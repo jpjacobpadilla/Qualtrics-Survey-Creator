32dab1986188f536f0fbed91ffd76a07b376c09d
Jacob Padilla <jp@jacobpadilla.com>
2023-02-09 17:40:28 -0500

    added body

--- survey_creator/mixins/matrix_question.py
+++ survey_creator/mixins/matrix_question.py
@@ -1,3 +1,52 @@
 class MatrixQuestionMixin:
-    def add_matrix_question():
+    def add_matrix_question() -> None:
         pass
+
+    @staticmethod
+    def _create_matrix_body() -> dict:
+        return {
+            'QuestionText': 'On a scale of 1-7, to what extent is Person 1 ________?',
+            'DefaultChoices': False,
+            'DataExportTag': '51_conv_like_p1',
+            'QuestionType': 'Matrix',
+            'Selector': 'Likert',
+            'SubSelector': 'SingleAnswer',
+            'DataVisibility': {
+                'Private': False,
+                'Hidden': False
+                },
+            'Configuration': {
+                'QuestionDescriptionOption': 'UseText',
+                'TextPosition': 'inline',
+                'ChoiceColumnWidth': 25,
+                'RepeatHeaders': 'none',
+                'WhiteSpace': 'OFF',
+                'MobileFirst': True
+                },
+            'QuestionDescription': 'On a scale of 1-7, to what extent is Person 1 ________?',
+            'Choices': {
+                '2': {
+                    'Display': 'likable'
+                    },
+                '3': {
+                    'Display': 'friendly'
+                    },
+                '4': {
+                    'Display': 'someone you would enjoy chatting with'
+                    },
+                '5': {
+                    'Display': 'someone you would like to be friends with'
+                    }
+                },
+            'ChoiceOrder': ['2', '3', '4', '5'],
+            'Validation': {
+                'Settings': {
+                    'ForceResponse': 'ON',
+                    'ForceResponseType': 'ON',
+                    'Type': 'None'
+                    }
+                },
+            'Language': [],
+            'QuestionID': 'QID469',
+            'QuestionText_Unsafe': 'On a scale of 1-7, to what extent is Person 1 ________?'
+        }
