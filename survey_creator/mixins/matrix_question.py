0f4b9a5461c118c1153d8198a63140bba6f836cb
Jacob Padilla <jp@jacobpadilla.com>
2023-02-09 18:18:03 -0500

    got it to partially work

--- survey_creator/mixins/matrix_question.py
+++ survey_creator/mixins/matrix_question.py
@@ -1,9 +1,19 @@
+class matrix_options
+
+
+
 class MatrixQuestionMixin:
-    def add_matrix_question(self, question_text: str, desc: str) -> None:
-        body = self._text_description_question(
-                        qtext=question_text,
+    def __init__(self, *args, **kwargs):
+        matrix_options = matrix_options()
+        super.__init__(*args, **kwargs)
+
+    def add_matrix_question(self, option: int, question_text: str, desc: str) -> None:
+
+
+        body = self._matrix_question(
+                        text=question_text,
+                        desc=desc, 
                         data_export_tag=desc, 
-                        question_desc=desc, 
                     )
         
         resp = self._make_qualtrics_request(
@@ -16,50 +26,111 @@ class MatrixQuestionMixin:
         self.question_list.append(resp['result']['QuestionID'])
 
     @staticmethod
-    def _create_matrix_body(qtext: str, data_export_tag: str, question_desc: str) -> dict:
-        return {
-            'QuestionText': qtext,
-            'DefaultChoices': False,
-            'DataExportTag': data_export_tag,
-            'QuestionType': 'Matrix',
-            'Selector': 'Likert',
-            'SubSelector': 'SingleAnswer',
-            'DataVisibility': {
-                'Private': False,
-                'Hidden': False
-                },
-            'Configuration': {
-                'QuestionDescriptionOption': 'UseText',
-                'TextPosition': 'inline',
-                'ChoiceColumnWidth': 25,
-                'RepeatHeaders': 'none',
-                'WhiteSpace': 'OFF',
-                'MobileFirst': True
-                },
-            'QuestionDescription': question_desc,
-            'Choices': {
-                '2': {
-                    'Display': 'likable'
+    @matrix_options.add(1)
+    def matrix_question_1(text: str, desc, data_export_tag) -> dict:
+        """
+        "choices" are the vertical options (on the left)
+        "answers" are the horizontal options (on top)
+        """
+        return {"RecodeValues":{
+                    "1":"0",
+                    "2":"1",
+                    "3":"2",
+                    "4":"3",
+                    "5":"4",
+                    "6":"5",
+                    "7":"6",
                     },
-                '3': {
-                    'Display': 'friendly'
+                "Answers":{
+                    "1":{
+                        "Display": 'Happy'
                     },
-                '4': {
-                    'Display': 'someone you would enjoy chatting with'
+                    "2":{
+                        "Display": 'Friendly'
                     },
-                '5': {
-                    'Display': 'someone you would like to be friends with'
+                    "3":{
+                        "Display": 'Someone you would enjoy chatting with'
+                    },
+                    "4":{
+                        "Display": 'Calm'
+                    },
+                    "5":{
+                        "Display": 'someone you would like to be friends with'
+                    },
+                    "6":{
+                        "Display": 'Likeable'
+                    },
+                    "7":{
+                        "Display": 'Sad'
+                    },
+                    "8":{
+                        "Display": 'Excited'
                     }
+                    },
+                    "AnswerOrder":[
+                    1,
+                    2,
+                    3,
+                    4,
+                    5,
+                    6,
+                    7,
+                    8
+                    ],
+                "ChoiceDataExportTags":False,
+                "ChoiceOrder":[
+                    1,
+                    2,
+                    3,
+                    4,
+                    5,
+                    6,
+                    7
+                    ],
+                "Choices":{
+                    "1":{
+                        "Display": '1-Not At All'
+                    },
+                    "2":{
+                        "Display": '2'
+                    },
+                    "3":{
+                        "Display": '3'
+                    },
+                    "4":{
+                        "Display": '4'
+                    },
+                    "5":{
+                        "Display": '5'
+                    },
+                    "6":{
+                        "Display": '6'
+                    },
+                    "7":{
+                        "Display": '7-Extremely'
+                    },
                 },
-            'ChoiceOrder': ['2', '3', '4', '5'],
-            'Validation': {
-                'Settings': {
-                    'ForceResponse': 'ON',
-                    'ForceResponseType': 'ON',
-                    'Type': 'None'
+                "Configuration":{
+                    "QuestionDescriptionOption":"UseText",
+                    "TextPosition":"inline",
+                    "ChoiceColumnWidth":25,
+                    "RepeatHeaders":"none",
+                    "WhiteSpace":"OFF",
+                    "MobileFirst":True,
+                    "ChoiceColumnWidthPixels":392
+                    },
+                "DefaultChoices":False,
+                "Language":[],
+                "QuestionDescription":desc,
+                "QuestionText":text,
+                "QuestionType":"Matrix",
+                "Selector":"Likert",
+                "SubSelector":"SingleAnswer",
+                "Validation":{"Settings":{
+                        "ForceResponse":"ON",
+                        "ForceResponseType":"ON",
+                        "Type":"None"
                     }
-                },
-            'Language': [],
-            'QuestionID': 'QID469',
-            'QuestionText_Unsafe': qtext
-        }
+                    },
+                "DataExportTag":data_export_tag
+                }
