8b3e7ba78fa7925f62a0b4ed8914c00b5ca75022
Jacob Padilla <jp@jacobpadilla.com>
2023-02-10 20:01:55 -0500

    added type to ac desc

--- survey_creator/mixins/attention_check_question.py
+++ survey_creator/mixins/attention_check_question.py
@@ -7,8 +7,16 @@ class AttentionCheckQuestionMixin(BaseMultipleChoiceQuestion):
         self.attention_check_num: int = 1
         super().__init__(*args, **kwargs)
    
-    def add_attention_check(self, question_text: str, choices: list) -> dict:
-        desc = f'Attention_check_{self.attention_check_num}'
+    def add_attention_check(self, question_text: str, choices: list, type: str) -> dict:
+        """
+        Add a multiple choice style attention check.
+
+        The TYPE (str) parameter is used to specify the attention check "Type". 
+        This is helpful in determining the answers to the attention checks when 
+        once you have collected the data.
+        """
+
+        desc = f'AC_{self.attention_check_num}_TYPE_{type}'
         self.attention_check_num += 1
 
         body = self._multiple_choice_question(
